873388a2495354265c16d2ebdf6be377a705d48c
Damien Chagnaud <damien-chagnaud@wanadoo.fr>
2026-05-17 15:06:21 -0400

    Update app_conf.php

--- conf/app_conf.php
+++ conf/app_conf.php
@@ -6,7 +6,7 @@
 return array(
     'app_name' => getenv('COPILOC_APP_NAME') ?: 'Copiloc Data API',
     'version' => getenv('COPILOC_APP_VERSION') ?: '1.0.0',
-    'site_url' => getenv('COPILOC_SITE_URL') ?: 'https://copiloc-data',
+    'site_url' => getenv('COPILOC_SITE_URL') ?: '',
     'database_system' => getenv('COPILOC_DATABASE_SYSTEM') ?: 'mariadb',
     'debug' => getenv('COPILOC_DEBUG') ? filter_var(getenv('COPILOC_DEBUG'), FILTER_VALIDATE_BOOLEAN) : false,
 );
